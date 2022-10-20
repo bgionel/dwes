926c32bbfcb15731e1a11b942478c50c3432b861
Bianca Ionel <biancagionel@gmail.com>
2022-10-20 14:04:34 +0100

    modificacion

--- data/ejercicios/cookies.php
+++ data/ejercicios/cookies.php
@@ -2,13 +2,20 @@
 
 //setcookie("primeracookie" , "aceptado", strtotime("+1 week"));
 
-$miarray = ["hola",25,[1,3,5]];
+$miarray = ['a'=> "hola", 'b'=>25,'c'=>[1,3,5]];
 $infoguardar = serialize($miarray);
+
+//$infojson = json_encode($miarray);
+$infojson = json_encode($miarray);
+echo "<br>Codificacion del array en JSON: " . $infojson;
+
 
 setcookie("primeracookie","aceptado", time()+300);
 setcookie("segundacookie",$infoguardar, time()+600);
 echo "<h1>Mi primera cookie</h1>";
 echo "Valor de la cookie uno : " . $_COOKIE["primeracookie"];
 echo "<pre>";
-var_dump(unserialize($infoguardar));
+echo "<br>Valor de cookie 2: ";
+//var_dump($infojson);
+var_dump(unserialize($_COOKIE["segundacookie"]));
 echo "<br> Numero de cookies creadas " . count($_COOKIE);
---
+++ data/ejercicios/ejercicioCookies.php
@@ -0,0 +1,14 @@
+<?php
+
+/* Crear web en la que selecciones entre 3 idiomas: español, ingles y aleman
+y  selecciones una marca de coche preferida y muestre el siguiente mensaje
+
+    Bienvenido querido usuario.
+    Welcome dear user.
+
+y la marca de coche seleccionada.
+
+    Esta informacion se debe guardar en cookies
+    Si no se selecciona idioma por defecto ser español
+*/
+
