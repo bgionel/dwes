721cc2cc90ef38ac17621b547e8e70cdcf3e9586
Bianca Ionel <biancagionel@gmail.com>
2022-11-04 23:41:04 +0000

    ejercicios

--- data/ejercicios/solucionest2/ejercicio6.php
+++ data/ejercicios/solucionest2/ejercicio6.php
@@ -1,58 +1,33 @@
-<?php
-
-if (isset($_REQUEST['lista'])) {
-    $lista = $_REQUEST['lista'];
-} else {
-    $lista = array();
-    // $lista = [];
-}
-
-if (isset($_REQUEST['nuevo'])) {
-    $nuevo = $_REQUEST['nuevo'];
-    $lista[] = $nuevo;
-}
-
-echo "<pre>";
-var_dump($lista);
-var_dump($nuevo);
-echo "</pre>";
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Ejercicio 6</title>
 </head>
 <body>
-    <h1>Lista de jugadores:</h1>    
-    <!-- //usar foreach para construir una lista ul>li -->
+    <h1>Equipo de basket:</h1> 
+    <?php
+    //creo el array equipo con array()
+    $equipo = array('Lebron James', 'Ricky Rubio', 'Pau Gasol', 'Marc Gasol', 'Stephen Curry');
+    //creo el array equipoBasket con []
+    $equipoBasket = ['Lebron James', 'Ricky Rubio', 'Pau Gasol', 'Marc Gasol', 'Stephen Curry']; 
+    ?>
     <ul>
-    <?php   
-    // lista de lo que ya existe:
-    foreach ($lista as $jugador) {
-        echo "<li>$jugador</li>";
+    <?php 
+    //recorro con foreach el array equipo y voy imprimiendo los jugadores en elementos li
+    foreach($equipo as $jugador){
+        echo "<li> $jugador </li>";
     }
     ?>
     </ul>
-
-    <h1>Nuevo jugador:</h1>
-    <!-- //construir formulario: -->
-        <form action="" method="get">
-
-        <?php   
-        //lista de lo que ya existe:
-        foreach ($lista as $jugador) {
-            echo '<input type="hidden" name="lista[]" value="' . $jugador . '">';
-        }
-        ?>
-
-        <label for="nuevo">Nuevo jugador: </label>
-        <input type="text" name="nuevo" value=""/>
-        <br>
-        <input type="submit">
-        
-        </form>        
+    <?php 
+    //recorro con for el array equipoBasket y voy imprimiendo los jugadores en elementos li junto con su posicion
+    for($i = 0 ; $i < count($equipoBasket); $i++){
+        echo "<li> Jugador " . $i . ": " . $equipoBasket[$i] . " </li>";
+    }
+    ?>
+    </ul>
 </body>
 </html>
